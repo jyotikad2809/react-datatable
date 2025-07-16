61ed2da6b9d35858a89188bd10380b8e7ca9505d
jyoti kad sharama <jyotikad2809@gmail.com>
2025-07-16 19:24:27 +0530

    fix: break the code and make small reusable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Styles.css";
-
+import Datatable from "./components /Datatable";
 const tableHeader = [
   { id: 0, label: "ID", key: "id" },
   { id: 1, label: "NAME", key: "firstName" },
@@ -40,106 +40,119 @@ export default function App() {
 
     fetchUsers();
   }, []);
-  const onChangeLimit = (e) => {
-    setLimit(Number(e.target.value));
-    setPage(1);
-  };
-  const filteredUsers = users.filter((user) => {
-    return headers.every((th) => {
-      const key = th.key;
-      const filterValue = (query[key] || "").trim().toLowerCase();
-
-      if (!filterValue) return true;
-
-      const userValue = String(user[key] || "").toLowerCase();
-      return userValue.includes(filterValue);
-    });
-  });
-
-  const start = (page - 1) * limit;
-  const end = start + limit;
-  const paginatedUsers = filteredUsers.slice(start, end);
-  const totalPages = Math.ceil(filteredUsers.length / limit);
-  const pageButtons = [];
-  for (let i = 1; i <= totalPages; i++) {
-    pageButtons.push(
-      <button
-        key={i}
-        onClick={() => setPage(i)}
-        style={{ fontWeight: page === i ? "bold" : "normal" }}
-      >
-        {i}
-      </button>
-    );
-  }
 
   return (
-    <div className="container">
-      <h1>Data Table</h1>
-
-      {error && (
-        <>
-          <p style={{ color: "red" }}>Error: {error.message}</p>
-        </>
-      )}
-      {loading && <p>...Loading</p>}
-
-      {!loading && !error && (
-        <table>
-          <thead>
-            <tr>
-              {headers.map((th) => (
-                <th key={th.id}>{th.label} </th>
-              ))}
-            </tr>
-            <tr>
-              {headers.map((th) => (
-                <th key={th.id}>
-                  <input
-                    type="text"
-                    placeholder={`Search ${th.label}`}
-                    value={query[th.key]}
-                    onChange={(e) => {
-                      setQuery((prev) => ({
-                        ...prev,
-                        [th.key]: e.target.value,
-                      }));
-                    }}
-                  />
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {paginatedUsers.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.firstName}</td>
-                <td>{user.username}</td>
-                <td>{user.email}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-
-      <div className="footer">
-        <select value={limit} onChange={onChangeLimit}>
-          <option value={5}>5</option>
-          <option value={10}>10</option>
-          <option value={20}>20</option>
-        </select>
-        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
-          Prev
-        </button>
-        {pageButtons}
-        <button
-          onClick={() => setPage(page + 1)}
-          disabled={page === totalPages}
-        >
-          Next
-        </button>
-      </div>
+    <div>
+      <Datatable
+        users={users}
+        headers={headers}
+        error={error}
+        errorMessage={"error in fetching data"}
+        loading={loading}
+      />
     </div>
   );
+
+  // const onChangeLimit = (e) => {
+  //   setLimit(Number(e.target.value));
+  //   setPage(1);
+  // };
+  // const filteredUsers = users.filter((user) => {
+  //   return headers.every((th) => {
+  //     const key = th.key;
+  //     const filterValue = (query[key] || "").trim().toLowerCase();
+
+  //     if (!filterValue) return true;
+
+  //     const userValue = String(user[key] || "").toLowerCase();
+  //     return userValue.includes(filterValue);
+  //   });
+  // });
+
+  // const start = (page - 1) * limit;
+  // const end = start + limit;
+  // const paginatedUsers = filteredUsers.slice(start, end);
+  // const totalPages = Math.ceil(filteredUsers.length / limit);
+  // const pageButtons = [];
+  // for (let i = 1; i <= totalPages; i++) {
+  //   pageButtons.push(
+  //     <button
+  //       key={i}
+  //       onClick={() => setPage(i)}
+  //       style={{ fontWeight: page === i ? "bold" : "normal" }}
+  //     >
+  //       {i}
+  //     </button>
+  //   );
+  // }
+
+  // return (
+  //   <div className="container">
+  //     <h1>Data Table</h1>
+
+  //     {error && (
+  //       <>
+  //         <p style={{ color: "red" }}>Error: {error.message}</p>
+  //       </>
+  //     )}
+  //     {loading && <p>...Loading</p>}
+
+  //     {!loading && !error && (
+  //       <table>
+  //         <thead>
+  //           <tr>
+  //             {headers.map((th) => (
+  //               <th key={th.id}>{th.label} </th>
+  //             ))}
+  //           </tr>
+  //           <tr>
+  //             {headers.map((th) => (
+  //               <th key={th.id}>
+  //                 <input
+  //                   type="text"
+  //                   placeholder={`Search ${th.label}`}
+  //                   value={query[th.key]}
+  //                   onChange={(e) => {
+  //                     setQuery((prev) => ({
+  //                       ...prev,
+  //                       [th.key]: e.target.value,
+  //                     }));
+  //                   }}
+  //                 />
+  //               </th>
+  //             ))}
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           {paginatedUsers.map((user) => (
+  //             <tr key={user.id}>
+  //               <td>{user.id}</td>
+  //               <td>{user.firstName}</td>
+  //               <td>{user.username}</td>
+  //               <td>{user.email}</td>
+  //             </tr>
+  //           ))}
+  //         </tbody>
+  //       </table>
+  //     )}
+
+  //     <div className="footer">
+  //       <select value={limit} onChange={onChangeLimit}>
+  //         <option value={5}>5</option>
+  //         <option value={10}>10</option>
+  //         <option value={20}>20</option>
+  //       </select>
+  //       <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+  //         Prev
+  //       </button>
+  //       {pageButtons}
+  //       <button
+  //         onClick={() => setPage(page + 1)}
+  //         disabled={page === totalPages}
+  //       >
+  //         Next
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
 }
